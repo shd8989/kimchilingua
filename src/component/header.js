c9d5918f98ca479cfd9f17d9e84ca947bd59d932
hdseo <terranpushs@naver.com>
2021-11-28 23:33:22 +0900

    top UI 변경 및 메인 이미지 적용

--- src/component/header.js
+++ src/component/header.js
@@ -1,10 +1,13 @@
+import kimchHome from '../assets/img/kimchilingua_home.jpg'
+// import { url } from "inspector";
+
 function Header() {
     return (
         <>
-        <header class="masthead">
+        <header class="masthead" style={{backgroundImage: `url(${kimchHome})`, marginTop: 75}}>
             <div class="container">
-                <div class="masthead-subheading">Bienvenidos a Kimchilingua!</div>
-                <div class="masthead-heading text-uppercase">Mucho gusto!</div>
+                <div class="masthead-subheading" style={{color: '#2e3383'}}>Bienvenidos a Kimchilingua!</div>
+                <div class="masthead-heading text-uppercase" style={{color: '#2e3383'}}>Mucho gusto!</div>
                 <a class="btn btn-xl text-uppercase" style={{color:'#fff',backgroundColor:'#2e3383',border:'#2e3383'}} href="#services">Dime más</a>
             </div>
         </header>
