bd720fec0ec29352494c0fff56bbc69df9b22747
hdseo <terranpushs@naver.com>
2021-12-09 02:38:16 +0900

    메인 화면 폰트 적용

--- src/component/header.js
+++ src/component/header.js
@@ -1,5 +1,4 @@
 import kimchHome from '../assets/img/kimchilingua_home.jpg'
-// import { url } from "inspector";
 
 function Header() {
     return (
@@ -7,7 +6,7 @@ function Header() {
         <header class="masthead" style={{backgroundImage: `url(${kimchHome})`, backgroundSize: '100% auto'}}>
             <div class="container">
                 <div class="masthead-subheading" style={{color: '#2e3383'}}>Bienvenidos a Kimchilingua!</div>
-                <div class="masthead-heading text-uppercase" style={{color: '#2e3383'}}>안녕하세요</div>
+                <div class="masthead-heading text-uppercase" style={{color: '#2e3383', fontFamily: 'enjoy_bold'}}>안녕하세요</div>
                 <a class="btn btn-xl text-uppercase" style={{color:'#fff',backgroundColor:'#2e3383',border:'#2e3383'}} href="#services">Ver más</a>
             </div>
         </header>
