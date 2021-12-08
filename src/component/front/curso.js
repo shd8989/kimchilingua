c163a7c857d2f94c1f5d3cfcd4adf34bb41bbd69
hdseo <terranpushs@naver.com>
2021-12-09 02:28:19 +0900

    curso 상세 문구 데이터 타입 변경 string->map

--- src/component/front/curso.js
+++ src/component/front/curso.js
@@ -1,5 +1,4 @@
 import basico1 from '../../assets/img/curso/Básico1.jpg'
-import basico2 from '../../assets/img/curso/Básico-2.jpg'
 import basico3 from '../../assets/img/curso/Básico-3.jpg'
 import intermedio from '../../assets/img/curso/Intermedio.jpg'
 import introductorio from '../../assets/img/curso/Introductorio-1.jpg'
@@ -8,68 +7,51 @@ import topik from '../../assets/img/curso/Topik-1.jpg'
 import { useState } from "react";
 import Modal from './modal';
 
-const message = "NIVEL INTRODUCTORIO \
-\
-Bimestre 1: 『EPS TOPIK』 1-7 \
-Este nivel está dirigido para las personas que estudian coreano por primera vez. Se inician en el conocimiento del alfabeto coreano Hangul(한글), las sílabas y la estructura de frase, saludos, agradecimiento y auto-presentación.  \
-\
-NIVEL BÁSICO \
-\
-Bimestre 1: 『EPS TOPIK』  8-12 \
-En este nivel se estudia la gramática y el vocabulario más básico guiado a las expresiones cotidianas y aprenderán las herramientas para poner comprender la lengua oral y escrita de forma más sencilla. \
-Aprenderán numerales: Fecha, la hora y contéo. \
-\
-Bimestre 2: 『EPS TOPIK』  13-17 \
-En este nivel aprenderán expresiones más practicas: hacer citas, pedir comida en restaurantes, espresiones relacionadas con el clima, pasatiempos y viajes. \
-\
-Bimestre 3: 『EPS TOPIK』  18-21 \
-\
-En este nivel aprenderán cómo usar transportes públicos, típicas llamadas telefónicas, compras por Internet, expresiones al colaborar con los compañeros y de invitaciones.  \
-\
-Bimestre 4: 『EPS TOPIK』 22-26\
-\
-En este nivel aprenderán cómo hablar de prohibiciones, modales y etiquetas de Corea, honoríficos, estudios y el cuerpo humano. \
-\
-Bimestre 5: 『EPS TOPIK』  27-30\
-En este nivel aprenderán conversaciones en lugares como hospitales y oficinas gubernamentales. \
-\
-NIVEL INTERMEDIO \
-\
-Bimestre 1: 『Seogang』 3A 1-8\
-El contenido varia depende de los estudiantes\
-Bimestre 2: 『Seogang』  3B 1-4\
-El contenido varia depende de los estudiantes\
-\
-\
-Bimestre 3: 『Seogang』  3B 5-8\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 4: 『Seogang』  4A 1-4\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 5: 『Seogang』  4A 5-8\
-El contenido varia depende de los estudiantes\
-\
-NIVEL AVANZADO  \
-\
-Bimestre 1: 『Seogang』  4B 1-4\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 2: 『Seogang』  4B 5-8\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 3: 『Seogang』  5A 1-4\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 4: 『Seogang』  5A 5-8\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 5: 『Seogang』  5B 1-4\
-El contenido varia depende de los estudiantes\
-\
-Bimestre 6: 『Seogang』  5B 5-8\
-El contenido varia depende de los estudiantes\
-";
+const message = [
+    {
+        "nivel":"NIVEL INTRODUCTORIO",
+        "title":[{"1":"Bimestre 1"}],
+        "subtitle":[{"1":"『EPS TOPIK』 1-7"}],
+        "content":[{"1":"Este nivel está dirigido para las personas que estudian coreano por primera vez. Se inician en el conocimiento del alfabeto coreano Hangul(한글), las sílabas y la estructura de frase, saludos, agradecimiento y auto-presentación."}]
+    },
+    {
+        "nivel":"NIVEL BÁSICO",
+        "title":[{"1":"Bimestre 1", "2":"Bimestre 2", "3":"Bimestre 3", "4":"Bimestre 4", "5":"Bimestre 5"}],
+        "subtitle":[{"1":"『EPS TOPIK』  8-12", "2":"『EPS TOPIK』  13-17"}, {"3":"『EPS TOPIK』  18-21"}, {"4":"『EPS TOPIK』  22-26"}, {"5":"『EPS TOPIK』  27-30"}],
+        "content":[
+            {"1":"En este nivel se estudia la gramática y el vocabulario más básico guiado a las expresiones cotidianas. Aprenderán las herramientas para poder comprender la lengua oral y escrita de forma más sencilla. Aprenderán numerales: Fecha, hora y contéo."},
+            {"2":"En este nivel aprenderán expresiones más prácticas: hacer citas, pedir comida en restaurantes, expresiones relacionadas con el clima, pasatiempos y viajes."},
+            {"3":"En este nivel aprenderán cómo usar el transporte público, llamadas telefónicas comunes, compras por Internet, expresiones al colaborar con compañeros y para hacer invitaciones."},
+            {"4":"En este nivel aprenderán cómo hablar de prohibiciones, modales y normas de etiquetas de Corea, honoríficos, estudios y el cuerpo humano."},
+            {"5":"En este nivel aprenderán cómo entablar conversaciones en lugares como hospitales y oficinas gubernamentales."}
+        ]
+    },
+    {
+        "nivel":"NIVEL INTERMEDIO",
+        "title":[{"1":"Bimestre 1", "2":"Bimestre 2", "3":"Bimestre 3", "4":"Bimestre 4", "5":"Bimestre 5"}],
+        "subtitle":[{"1":"『『Seogang』 3A 1-8", "2":"『『Seogang』 3B 1-4"}, {"3":"『『Seogang』 3B 5-8"}, {"4":"『『Seogang』 4A 1-4"}, {"5":"『『Seogang』 4A 5-8"}],
+        "content":[
+            {"1":"El contenido varía dependiendo del nivel y necesidades de los estudiantes"},
+            {"2":"El contenido varía dependiendo del nivel y necesidades de los estudiantes"},
+            {"3":"El contenido varía dependiendo del nivel y necesidades de los estudiantesBimestre"},
+            {"4":"El contenido varía dependiendo del nivel y necesidades de los estudiantes"},
+            {"5":"El contenido varía dependiendo del nivel y necesidades de los estudiantes"}
+        ]
+    },
+    {
+        "nivel":"NIVEL AVANZADO",
+        "title":[{"1":"Bimestre 1", "2":"Bimestre 2", "3":"Bimestre 3", "4":"Bimestre 4", "5":"Bimestre 5", "6":"Bimestre 6"}],
+        "subtitle":[{"1":"『『Seogang』 4B 1-4", "2":"『『Seogang』 4B 5-8"}, {"3":"『『Seogang』 5A 1-4"}, {"4":"『『Seogang』 5A 5-8"}, {"5":"『『Seogang』 5B 1-4"}, {"6":"『『Seogang』 5B 5-8"}],
+        "content":[
+            {"1":"El contenido varía dependiendo de los estudiantes"},
+            {"2":"El contenido varía dependiendo de los estudiantes"},
+            {"3":"El contenido varía dependiendo de los estudiantes"},
+            {"4":"El contenido varía dependiendo de los estudiantes"},
+            {"5":"El contenido varía dependiendo de los estudiantes"},
+            {"6":"El contenido varía dependiendo de los estudiantes"}
+        ]
+    },
+];
 
 function Curso() {
     const [modalVisible, setModalVisible] = useState(false)
@@ -79,6 +61,7 @@ function Curso() {
     const closeModal = () => {
         setModalVisible(false)
     }
+    console.log(message)
     return (
         <>
         <section class="page-section bg-light" id="portfolio">
@@ -92,7 +75,8 @@ function Curso() {
                             visible={modalVisible}
                             closable={true}
                             maskClosable={true}
-                            onClose={closeModal}>{message}</Modal>
+                            onClose={closeModal}
+                            message={message}></Modal>
                         }
                     </div>
                 </div>
